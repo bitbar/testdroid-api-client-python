77e316c037c90b4c0c2067ef818c6034dc7db028
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2014-11-03 16:01:44 -0800

    Added missing meta-data:author_email

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ Testdroid API client for Python""",
         'Intended Audience :: Developers'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='testdroid rest api client',
       author='Henri Kivelä <henri.kivela@bitbar.com>, Sakari Rautiainen <sakari.rautiainen@bitbar.com>',
+      author_email='info@bitbar.com'
       url='http://www.testdroid.com',
       license='Apacha License v2.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
