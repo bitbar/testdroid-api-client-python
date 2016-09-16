ca82d1f1e588f367cd6ec639d491c33a8414646b
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2016-09-16 09:51:42 -0700

    Update __init__.py

--- testdroid/__init__.py
+++ testdroid/__init__.py
@@ -7,7 +7,7 @@ from urlparse import urljoin
 from collections import namedtuple
 from datetime import datetime
 
-__version__ = '2.5.1'
+__version__ = '2.6.0'
 
 FORMAT = "%(message)s"
 logging.basicConfig(format=FORMAT)
