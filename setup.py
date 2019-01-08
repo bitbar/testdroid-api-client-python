c978bd4b8315a8f688dbe37273945afacfac36f2
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2019-01-08 12:31:34 -0800

    Version bump to 2.69.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 
-version = '2.61.0'
+version = '2.69.0'
 
 setup(name='testdroid',
       version=version,
@@ -14,7 +14,7 @@ Testdroid API client for Python""",
         'Topic :: Software Development',
         'Intended Audience :: Developers'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='testdroid rest api client',
-      author='Henri Kivelä <henri.kivela@bitbar.com>, Sakari Rautiainen <sakari.rautiainen@bitbar.com>, Teppo Malinen <teppo.malinen@bitbar.com>, Jarno Tuovinen <jarno.tuovinen@bitbar.com>',
+      author='Henri Kivelä <henri.kivela@bitbar.com>, Sakari Rautiainen <sakari.rautiainen@bitbar.com>, Teppo Malinen <teppo.malinen@bitbar.com>, Jarno Tuovinen <jarno.tuovinen@bitbar.com>, Atte Keltanen <atte.keltanen@bitbar.com>',
       author_email='info@bitbar.com',
       url='http://www.testdroid.com',
       license='Apache License v2.0',
